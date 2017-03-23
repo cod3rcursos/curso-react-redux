b6c97fae6462c8f73f08cde436be10f3f1cd1974
Leonardo Leitão <leonardomleitao@gmail.com>
2017-03-23 13:17:11 -0300

    correcao de responsividade

--- my-money-app/frontend/src/common/template/menuItem.jsx
+++ my-money-app/frontend/src/common/template/menuItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props => (
     <li> 
         <a href={props.path}> 
-            <i className={`fa fa-${props.icon}`}></i> {props.label}
+            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
         </a>
     </li>
 )
--- my-money-app/frontend/src/common/template/menuTree.jsx
+++ my-money-app/frontend/src/common/template/menuTree.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default props => (
     <li className='treeview'> 
-        <a href> 
-            <i className={`fa fa-${props.icon}`}></i> {props.label}
+        <a href={props.path}> 
+            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
             <i className='fa fa-angle-left pull-right'></i>
         </a>
         <ul className='treeview-menu'> 
