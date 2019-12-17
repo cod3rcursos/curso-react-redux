36dde16dd73ebff4bfc97691eba64dac857ed8c4
luiz ferrari <lferrari@daitan.com>
2019-12-17 13:35:59 -0300

    create configure store file

--- my-money-app/frontend/src/index.jsx
+++ my-money-app/frontend/src/index.jsx
@@ -1,20 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { applyMiddleware, createStore } from 'redux'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import Routes from "./main/routes";
+import store from "./configureStore";
 
-import promise from 'redux-promise'
-import multi from 'redux-multi'
-import thunk from 'redux-thunk'
-
-import Routes from './main/routes'
-import reducers from './main/reducers'
-
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
-      && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
-    <Provider store={store}>
-        <Routes />
-    </Provider>
-, document.getElementById('app'))
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById("app")
+);
