17b534daa04b9c598715c7363eb64a2d0cdd1aa2
luiz ferrari <lferrari@daitan.com>
2019-12-17 16:38:18 -0300

    refacto grid.jsx

--- my-money-app/frontend/src/common/layout/grid.jsx
+++ my-money-app/frontend/src/common/layout/grid.jsx
@@ -3,15 +3,11 @@ import React, { Component } from "react";
 export default class Grid extends Component {
 
   toCssClasses(numbers) {
-    const cols = numbers ? numbers.split(" ") : [];
-    let classes = "";
-
-    if(cols[0]) classes += `col-xs-${cols[0]}`; 
-    if(cols[1]) classes += ` col-sm-${cols[1]}`;
-    if(cols[2]) classes += ` col-md-${cols[2]}`;
-    if(cols[3]) classes += ` col-lg-${cols[3]}`;
-
-    return classes; 
+    const classes = ["col-xs", "col-sm", "col-md", "col-lg"];
+    const sizes = numbers ? numbers.split(" ") : [];
+    const toCssClass = (size, index) => `${classes[index]}-${size}`;
+    
+    return sizes.map(toCssClass).join(" ");
   }
 
   render() {
