5d44c38da467e91c06aa4f6787b45590961f8185
luiz ferrari <lferrari@daitan.com>
2019-12-17 13:50:26 -0300

    add dotenv on backend

--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -1,11 +1,19 @@
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+const mongoose = require("mongoose");
+const connUrl = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
-mongoose.Error.messages.Number.min = 
-    "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
-mongoose.Error.messages.Number.max = 
-    "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
-mongoose.Error.messages.String.enum = 
-    "'{VALUE}' não é válido para o atributo '{PATH}'."
+mongoose.Promise = global.Promise;
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório.";
+
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'.";
+
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'.";
+
+mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'.";
+
+console.log(connUrl);
+
+module.exports = mongoose.connect(connUrl, {
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS
+});
--- my-money-app/backend/src/loader.js
+++ my-money-app/backend/src/loader.js
@@ -1,3 +1,7 @@
-const server = require('./config/server')
-require('./config/database')
-require('./config/routes')(server)
+require("dotenv").config();
+require("./config/database");
+
+const server = require("./config/server");
+const routes = require("./config/routes");
+
+routes(server);
